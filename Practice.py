83bb71d6db08e998557c26d1b9ce0b9b813fe23c
Kush Yadav <122624609+kushyadavgit@users.noreply.github.com>
2025-07-22 13:07:48 +0530

    first commit

---
+++ Practice.py
@@ -0,0 +1,47 @@
+# x = ("hi i am practiceing python  from scrtch")
+
+# print(x)
+
+# Add two numbers.
+
+a = 10 
+b = 20 
+print (a+b)
+
+# Find the square and cube of a number.
+a = 10
+print(a**2)
+print(a**3)
+
+# Swap two variables (with and without a third variable).
+
+a = 10
+b =30 
+
+
+# Find the greatest of two numbers.
+a = 30
+b = 50
+if a>b :
+    print("a is greatest number ")
+else:
+    print("b is greatest")    
+# Find the greatest among three numbers.
+
+# Check if a number is even or odd
+a = int(3)
+if a %2 ==0:
+    print("a is even number  ")
+else:
+    print("not even ")
+
+
+# Check if a number is positive, negative, or zero.
+
+# Calculate the simple interest.
+
+# Calculate the area and circumference of a circle.
+
+# Convert temperature from Celsius to Fahrenheit.
+
+
---
+++ loop.py
@@ -0,0 +1,29 @@
+# Print numbers from 1 to 100.
+# for i in range(1,101):
+#     print(i)
+
+# Print even numbers between 1 to 50.
+# for i in range(1,51):
+#     if i %2==0:
+#         print(i)
+# Sum of all numbers from 1 to N.
+# n = int(input("enter your number = "))
+# for i  in range(1,n+1):
+#     print(i)
+# Factorial of a number using loop.
+n = 5
+fac= 1
+for i  in range (1,n+1):
+    fac *= i
+    print(fac)
+# Table of a number (e.g., 5 × 1 = 5 … 5 × 10 = 50).
+
+# Count digits in a number.
+
+# Reverse a number (e.g., 123 → 321).
+
+# Check if a number is a palindrome.
+
+# Sum of digits of a number.
+
+# Armstrong number check (e.g., 153 → 1³+5³+3³ = 153).
